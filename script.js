04e702341ecae007e784dada7ee93bec203c8ed8
Luiz Felipe Nunes <111444795+luizfelipenunes@users.noreply.github.com>
2026-03-10 15:44:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 let eventosGlobais = [];
 
+function pegarLogo(titulo){
+
+titulo = titulo.toLowerCase();
+
+if(titulo.includes("vasco")){
+return "https://upload.wikimedia.org/wikipedia/en/5/5b/CR_Vasco_da_Gama_logo.svg";
+}
+
+if(titulo.includes("celtics")){
+return "https://upload.wikimedia.org/wikipedia/en/8/8f/Boston_Celtics.svg";
+}
+
+if(titulo.includes("seahawks")){
+return "https://upload.wikimedia.org/wikipedia/en/8/8e/Seattle_Seahawks_logo.svg";
+}
+
+if(titulo.includes("f1")){
+return "https://upload.wikimedia.org/wikipedia/commons/3/33/F1.svg";
+}
+
+if(titulo.includes("motogp")){
+return "https://upload.wikimedia.org/wikipedia/commons/7/77/Moto_Gp_logo.svg";
+}
+
+if(titulo.includes("indy")){
+return "https://upload.wikimedia.org/wikipedia/commons/3/3e/IndyCar_Series_logo.svg";
+}
+
+if(titulo.includes("tenis")){
+return "https://upload.wikimedia.org/wikipedia/commons/3/3e/Tennis_Racket_and_Ball.svg";
+}
+
+return "";
+
+}
+
 fetch("eventos.json")
 .then(response => response.json())
 .then(data => {
@@ -24,9 +60,14 @@ return;
 
 let evento = eventosGlobais[0];
 
+let logo = pegarLogo(evento.titulo);
+
 destaque.innerHTML = `
-<div class="evento">
-<div class="titulo">${evento.titulo}</div>
+<div class="evento destaque">
+<div class="titulo">
+<img src="${logo}" class="logo">
+${evento.titulo}
+</div>
 <div class="hora">${evento.data}</div>
 <div class="transmissao">📺 ${evento.transmissao}</div>
 </div>
@@ -56,11 +97,16 @@ eventosFiltrados.sort((a,b)=>(a.prioridade||2)-(b.prioridade||2));
 
 eventosFiltrados.forEach(evento => {
 
+let logo = pegarLogo(evento.titulo);
+
 let card = document.createElement("div");
 card.className = "evento";
 
 card.innerHTML = `
-<div class="titulo">${evento.titulo}</div>
+<div class="titulo">
+<img src="${logo}" class="logo">
+${evento.titulo}
+</div>
 <div class="hora">${evento.data}</div>
 <div class="transmissao">📺 ${evento.transmissao}</div>
 `;
