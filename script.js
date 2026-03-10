6bbf44a115620a0285abedb4f5a5e27c714515c1
Luiz Felipe Nunes <111444795+luizfelipenunes@users.noreply.github.com>
2026-03-10 15:09:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ fetch("eventos.json")
 
 let agenda = document.getElementById("agenda");
 
+let esportes = {};
+
 data.forEach(evento => {
+
+if(!esportes[evento.esporte]){
+esportes[evento.esporte] = [];
+}
+
+esportes[evento.esporte].push(evento);
+
+});
+
+for(let esporte in esportes){
+
+agenda.innerHTML += `<h2>${esporte}</h2>`;
+
+esportes[esporte].forEach(evento => {
 
 agenda.innerHTML += `
 <div class="evento">
@@ -15,5 +31,7 @@ agenda.innerHTML += `
 `;
 
 });
+
+}
 
 });
